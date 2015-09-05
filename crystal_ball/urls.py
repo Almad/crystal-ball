e7ec2daddcdee55bdc6d457a29f248b4fd3d3fe4
Almad <bugs@almad.net>
2015-09-05 23:48:04 +0200

    Revert "Remove old crystal ball"
    This reverts commit 0b24004db55d87a5f3d6bdd5415c4dea2eced4e5.

---
+++ crystal_ball/urls.py
@@ -0,0 +1,14 @@
+""" Default urlconf for crystal_ball """
+
+from django.conf.urls import include, patterns, url
+from django.contrib import admin
+admin.autodiscover()
+
+import ball.urls
+
+urlpatterns = patterns('',
+    url(r'^admin/', include(admin.site.urls)),
+    url(r'^accounts/', include('allauth.urls')),
+    url(r'', include('base.urls')),
+)
+
