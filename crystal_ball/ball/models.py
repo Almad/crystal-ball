806e8eb07e86a9cb93978864f891705dcae5ea74
Almad <bugs@almad.net>
2015-09-05 21:06:57 +0200

    Properly restructure initial app

---
+++ crystal_ball/ball/models.py
@@ -0,0 +1,14 @@
+from django.db import models
+
+class Company(models.Model):
+    name = models.CharField(max_length=255)
+
+class Team(models.Model):
+    name = models.CharField(max_length=255)
+
+class GithubTeam(models.Model):
+    name = models.CharField(max_length=255)
+    team = models.ForeignKey(Team)
+
+class Member(models.Model):
+    date_joined = models.DateField('Date the Member joined the Company')
---
+++ crystal_ball/ball/urls.py
@@ -0,0 +1,10 @@
+"""Ball URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/1.8/topics/http/urls/
+"""
+
+from django.conf.urls import include, url
+
+urlpatterns = [
+]
